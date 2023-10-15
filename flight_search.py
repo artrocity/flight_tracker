e1b416810c92be9756e63680d93708dc4152e17d
Ryan Michael <35464934+artrocity@users.noreply.github.com>
2023-10-15 14:18:28 -0500

    Update flight_search.py
    Updated the file with doc strings
    Updated the flight search method and the return details, it now returns a list with all pertinent information instead of a giant JSON

--- flight_search.py
+++ flight_search.py
@@ -7,18 +7,32 @@ from dateutil.relativedelta import relativedelta
 
 class FlightSearch:
     def __init__(self) -> None:
-        self.fly_from = "SET YOUR DEPARTURE AIRPORT/AIRPORTS"
+        self.fly_from = "YOUR DEPARTURE AIRPORT"
         self.set_time()
-        self.API_KEY = "SET YOUR ENVIRONMENT VARIABLE API KEY HERE"
+        self.API_KEY = "YOUR ENV VARIABLE API KEY"
 
-    def set_time(self):
+    def set_time(self) -> None:
+        """
+        Summary:
+            Gets the date for tomorrow and formats the value and then stores it into a variable
+            Gets the date for 6 months from now and formats the value and then stores it into a variable
+        """
         tomorrow = (datetime.today() + timedelta(days=1))
         self.date_tomorrow = tomorrow.strftime("%d/%m/%Y")
 
         six_months = tomorrow + relativedelta(months=6)
         self.date_six_months = six_months.strftime("%d/%m/%Y")
 
-    def get_iata_codes(self, city):
+    def get_iata_codes(self, city:dict) -> str:
+        """
+        Summary:
+            Takes a string(city) as input and then loops through each city and queries the Kiwi API
+            to find all IATA airport codes for the listed city
+        Args:
+            city (dict): Takes a dictionary as parameter and then loops through accessing the 'location' key
+        Returns:
+            str: Returns a string, which is the cities IATA code.
+        """
         self.city = city
         self.search_endpoint = "https://tequila-api.kiwi.com/locations/query"
 
@@ -41,12 +55,20 @@ class FlightSearch:
         else:
             return None
         
-    def format_times(self, iso_time):
+    def format_times(self, iso_time) -> tuple:
+        """
+        Summary
+            Takes a time in ISO format and converts it to a more userfriendly time
+        Args:
+            iso_time (str): ISO formatted time
+        Returns:
+            tuple: formatted date and time 
+        """
         # Parse
         dt = datetime.fromisoformat(iso_time.replace("z", "+00:00"))
 
         # Set the timezone
-        local_timezone = pytz.timezone("US/Central")
+        local_timezone = pytz.timezone("US/Central") # Change your timezone here if you wish
         local_dt = dt.astimezone(local_timezone)
 
         # Format the Date and Time
@@ -55,11 +77,22 @@ class FlightSearch:
 
         return formatted_date, formatted_time
        
-    def search_for_flight(self, data):
+    def search_for_flight(self, data) -> list:
+        """
+        Summary:
+            Takes a dictionary as an argument and then formats the data into a query matching the dictionary data to 
+            parameters inside of the API request. 
+        Args:
+            data (dict): Spreadsheet data with the main dependency being the 'iataCode' key. 
+        Returns:
+            list: A list of all flight data for the flight search. 
+        """
         tequila_endpoint = "https://api.tequila.kiwi.com/v2/search"
         header = {
             "apikey" : self.API_KEY
         }
+
+        all_flights = []
 
         for entry in data:
             iata_code = entry["iataCode"]
@@ -88,6 +121,17 @@ class FlightSearch:
 
                 formatted_date, formatted_time = self.format_times(departure)
                 print(f"Found 1 flight, leaving {fly_from}, on {formatted_date} @ {formatted_time} flying to {fly_to} for {night_stay} nights for ${price}.")
+                flight_details = {
+                    "departure" : fly_from,
+                    "destination" : fly_to,
+                    "date" : formatted_date,
+                    "time" : formatted_time,
+                    "nights": night_stay,
+                    "price" : price
+                }
+                all_flights.append(flight_details)
+
+        return all_flights
             
 
         
